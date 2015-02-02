52a762b9b2166b192e98f431327273a2ba63e306
codewink <codewink@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 11:05:10 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/scripts-styles-debug-bar/trunk@1080797 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== What Scripts Styles Debug Bar ===
+Contributors: Ravinder Kumar, Sudeep Kumar Rana
+Tags: Debug
+Tested up to : 4.1
+Stable Tag: 1.0
+Dependencies : debug-bar plugin
+
+Adds two menu items into debug-bar menu: 1) All Scripts 2) All Styles
+
+== Descriptions ==
+
+A must for wordpress developers.
+
+Many times we need to know what all scripts and styles are being executing on a certain page. This
+
+little plugin can help you out. 
+
+It tells you 1) Enqueued 2) Script Handle 3) src link 4) version and 5) Dependencies of the script
+
+== How to use == 
+Install debug-bar plugin first.
+
+Install What-Scripts-Styles-Debug-Bar and it will get attached with debug-bar plugin.
+
+== 1.0 ==
+Initial Release
---
+++ scripts-styles-debug-bar.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Plugin Name: Scripts Styles Debug Bar
+ * Plugin URI: http://sudeeprana.com
+ * Description: A tiny plugin to display all enqueued scripts and styles. debug-bar Dependent. Little help for Plugin/Theme Developers.
+ * Version: 0.1
+ * Author: The CodeWink Team
+ * Author URI: http://sudeeprana.com
+ * License: GPL2
+*/
+
+/*
+
+    Copyright (C) Year  Author  Email
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+include "functions.php";
+?>
